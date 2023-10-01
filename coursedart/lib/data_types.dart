3ea9f1a11b0530e85f354e99706a17a3f1bb1f25
Wagdi Saif <WagdiSaif121@gmail.com>
2023-10-01 11:35:28 -0700

    Dart Course

--- coursedart/lib/data_types.dart
+++ coursedart/lib/data_types.dart
@@ -34,7 +34,111 @@ void main(List<String> args) {
   num del = 45255555522222222;
   print(del);
 
-  List cities = ["Sana'a ", "Taiz", "Aden", "Abb"]; //dynamic List
+  Runes heart = Runes('\u2665');
+//List
 
-  print(cities);
+  List cities = ["Sana'a ", "Taiz", "Aden", "Abb"]; //dynamic List
+  List<String> cities1 = ["Sana'a ", "Taiz", "Aden", "Abb"];
+  cities.add('Hagga');
+  cities.addAll(['Thamar', 'Marb']);
+  bool cityII = cities.any((element) => element == 'Aden'); //search any Element
+
+  List<int> number = [1, 3, 0, 2];
+  bool flag = number.every((element) => element > 1);
+
+  Iterable<int> iterableNum = [12, 34, 45, 67, 23, 43];
+  iterableNum = number.expand((element) sync* {
+    for (var i = 1; i <= element; i++) {
+      yield i;
+    }
+  });
+
+  var rstl = number.where((element) => element.isOdd);
+
+  final numbers = <int>[1, 2, 3, 5, 6, 7];
+
+  numbers.shuffle();
+  print(numbers);
+
+  final revers = numbers.reversed;
+  print(revers.toList());
+  var restWher =
+      numbers.firstWhere((element) => element > 3, orElse: (() => 0));
+  print(restWher);
+
+  var result = numbers.singleWhere(
+    (element) => element > 51,
+    orElse: () => 0,
+  );
+  print(result);
+
+  print(numbers.runtimeType);
+
+  var itretor = numbers.iterator;
+  for (; itretor.moveNext();) print(itretor.current);
+  int sum =
+      numbers.fold(1, (previousValue, element) => previousValue * element);
+
+  print(sum);
+
+  var planets = <String>['Earth', 'Jupiter'];
+  var updated = planets.followedBy(['Mars', 'Venus', 'san']);
+  print(updated); // (Earth, Jupiter, Mars, Venus,san)
+
+  numbers.insert(0, 20);
+  print(numbers); //
+  var getRang = numbers.getRange(0, 4);
+  print(getRang);
+  final skip = numbers.skip(2);
+  print(skip);
+  print(numbers.take(2));
+
+  print(numbers.reduce((value, element) => value + element));
+  numbers.retainWhere((element) => element > 5);
+  print(numbers);
+  numbers.setAll(1, [30, 40]);
+  print(numbers);
+  final algh = <String>['two', 'three', 'four'];
+  algh.sort((a, b) => a.length.compareTo(b.length));
+  print(algh); //
+
+  //map
+
+  Map<String, int> ages = {'Wagdi': 25, 'hamed': 30};
+  Map<String, dynamic> person = {'name': 'Wagdi', 'age': 25};
+  print(person);
+
+  print(person.entries.length);
+  print(person.keys.elementAt(1));
+
+  person.putIfAbsent('ID', () => 1233);
+
+  print(person);
+  print("values map are${person.values}");
+  print(person.containsKey('age'));
+  person.update('lastname', (value) => 'Saif', ifAbsent: () => 'Saif');
+  print(person);
+  person.forEach((key, value) {
+    print("key is ${key} value of key is ${value}");
+  });
+
+//set
+
+  Set<int> numbersSet = {1, 2, 3, 4, 5};
+  Set<double> temperaturesSet = {98.6, 99.1, 97.9};
+  Set<String> fruitsSet = {'apple', 'banana', 'orange'};
+  Set<bool> flagsSet = {true, false};
+  Set<dynamic> mixedSet = {1, 'two', true};
+
+  print("fruit set is ${fruitsSet}");
+  final search = numbersSet.lookup(10);
+
+  print(search);
+  final group1 = <String>{'Ahmed', 'Sam', 'Ali'};
+  final group2 = <String>{'Sam', 'hamood', 'Saif'};
+  final unionSet = group1.intersection(group2);
+  print(unionSet); // {Sam}
+  Set<String> allGroup = group1.union(group2);
+
+  print(allGroup);
 }
--- coursedart/lib/funaction.dart
+++ coursedart/lib/funaction.dart
@@ -1,13 +1,50 @@
 import 'dart:convert';
 import 'dart:io';
 
-void fun(int x) {
-  print('Value x is $x');
+double add(double x, double y) {
+  return x + y;
 }
 
-void main(List<String> args) {
-  print("Enter x values : ");
-  int? x = int.parse(stdin.readLineSync()!);
+void fun(int x, int y) {
+  print('Value x is ${x * y}');
+}
 
-  fun(x);
+int max(int x, int y) {
+  return x > y ? x : y;
+}
+
+dynamic max1(dynamic x, dynamic y) {
+  return x > y ? x : y;
+}
+
+void fullName(String firstName, Function() laterName) {
+  print(firstName + " " + laterName());
+}
+
+void showData(num ID, String firstName, [String? lastName]) {
+  print("ID is ${ID}\t Name is :${firstName}\t liastName   :${lastName}");
+}
+
+void showFullData(
+    {required int ID, required String firstName, String? lastName}) {
+  print("ID is $ID\t Name is: $firstName\t LastName: $lastName");
+}
+
+void main() {
+  // print("Enter x values : ");
+  // int? x = int.parse(stdin.readLineSync()!);
+  // print("Enter y values : ");
+  // int? y = int.parse(stdin.readLineSync()!);
+
+  // fun(x, y);
+  // print("Max number is  : ${max(x, y)}\n");
+  // print("Max number is  : ${max1(23.9, 33.9)}");
+  String firstName = "Wagdi";
+  String lastName = "Saif";
+  fullName(firstName, () {
+    return lastName;
+  });
+
+  showData(23344, "Wagdi");
+  showFullData(ID: 233344, firstName: "Wagdi");
 }
