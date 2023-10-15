d3d3550c1f6229239eb36359656b9ad977982555
Wagdi Saif <WagdiSaif121@gmail.com>
2023-10-15 07:50:48 -0700

    Dart Course

--- coursedart/bin/coursedart.dart
+++
@@ -1,5 +0,0 @@
-import 'package:coursedart/coursedart.dart' as coursedart;
-
-void main(List<String> arguments) {
-  print('Hello world: ${coursedart.calculate()}!');
-}
--- coursedart/test/coursedart_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:coursedart/coursedart.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
